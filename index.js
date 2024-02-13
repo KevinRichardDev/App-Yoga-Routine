ee9a287915fead3279767207c47206fd8c5969ac
KevinRichardDev <kevin.richard0@orange.fr>
2024-02-13 05:50:37 +0100

    handleEventMinutes

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ const utils = {
     main.innerHTML = content;
     document.querySelector(".btn-container").innerHTML = btn;
   },
+
+  handleEventMinutes: function() {
+    document.querySelectorAll('input[type="number"]').forEach((input) => {
+      input.addEventListener('input', (e) => {
+        exerciceArray.map((exo) => {
+          if(exo.pic == e.target.id) {
+            console.log("yes");
+          }
+        })
+      })
+    })
+  }
 };
 
 const page = {
@@ -46,6 +58,7 @@ const page = {
       "<ul>" + mapArray + "</ul>",
       "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
     );
+    utils.handleEventMinutes();
   },
 
   routine: function () {
